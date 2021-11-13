0664c852690cdb5a817b12d5b81cba72cc78aa47
peacedev89 <112556433+peacedev89@users.noreply.github.com>
2021-11-13 09:10:00 +0100

    s

--- app/components/session/destroy.js
+++ app/components/session/destroy.js
@@ -5,10 +5,7 @@ var hashHistory = require('react-router').hashHistory;
 var Logout = React.createClass({
 
 	//sign out from firebase, reroute to login page
-	componentDidMount() {
-		firebase.auth().signOut();
-		hashHistory.push('/login');
-	},
+	
 
 	render: function(){
 		return <p>Logged out</p>;
