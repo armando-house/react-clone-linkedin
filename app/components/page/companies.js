fd600b0ee831ae146837cdf831dcaa37c55fcd55
peacedev89 <112556433+peacedev89@users.noreply.github.com>
2021-12-13 09:10:00 +0100

    s

--- app/components/page/companies.js
+++ app/components/page/companies.js
@@ -13,7 +13,12 @@ var Companies = React.createClass({
 
 	componentWillMount: function(){
 		this.companyRef = firebase.database().ref('users').orderByChild("recruiter").equalTo(true);
-		
+		this.companyRef.on("child_added", snap=>{
+			company = snap.val();
+			company.user_id = snap.ref.key;
+			this.state.companies.push(company);
+			this.setState({companies: this.state.companies});
+		});
 	},
 
 	componentWillUnmount: function(){
